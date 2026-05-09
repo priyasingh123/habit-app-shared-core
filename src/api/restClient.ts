da5ee91b69decd5ac277d811d0aed946f599310c
priya <priyaasinghh345@gmail.com>
2026-05-09 22:42:14 +0530

    add restclient file

---
+++ src/api/restClient.ts
@@ -0,0 +1,38 @@
+const baseUrl = process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:8081";
+
+interface restApiRequestParams {
+  endpoint: string;
+  options: RequestInit;
+}
+const restApiRequest = async ({
+  endpoint,
+  options = {},
+}: restApiRequestParams) => {
+  const url = `${baseUrl}${endpoint}`;
+
+  try {
+    const res = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        ...(options?.headers || {}),
+      },
+      ...options,
+    });
+    if (!res.ok) {
+      const errorData = await res.json();
+      const message =
+        errorData?.message ||
+        errorData?.error ||
+        `Request failed with status ${res.status}`;
+      throw new Error(message);
+    }
+    return res.json();
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error("API request error:", err.message);
+    } else {
+      console.error("API request error:", err);
+    }
+    throw err;
+  }
+};
